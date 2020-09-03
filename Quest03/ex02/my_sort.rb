355ef9d4526aefd29092f1b10064711495902f5f
MomohNobert <okhai0112@gmail.com>
2020-09-03 16:29:53 +0100

    minor changes

--- Quest03/ex02/my_sort.rb
+++ Quest03/ex02/my_sort.rb
@@ -8,10 +8,10 @@ def my_sort(array, order)
       end
     end
   
-    if order == ":asc"
+    if order == :asc
       array = array.sort
       return array
-    elsif order == ":desc"
+    elsif order == :desc
       array = array.sort { |a,b| b <=> a}
       return array
     else 
--- Quest03/ex06/my_robot_simulator.rb
+++ Quest03/ex06/my_robot_simulator.rb
@@ -1,47 +1,47 @@
 def run_robot(instructions)
-    initial_state = {x: 0, y: 0, bearing: "north"}
+    initial_state = {x: 0, y: 0, bearing: :north}
   
     possible_actions = instructions.downcase.split("")
   
     possible_actions.map do |p|
       if p == "l"
-        if initial_state[:bearing] == "north"
-          initial_state[:bearing] = "west"
+        if initial_state[:bearing] == :north
+          initial_state[:bearing] = :west
           next
-        elsif initial_state[:bearing] == "east"
-          initial_state[:bearing] = "north"
+        elsif initial_state[:bearing] == :east
+          initial_state[:bearing] = :north
           next
-        elsif initial_state[:bearing] == "south"
-          initial_state[:bearing] = "east"
+        elsif initial_state[:bearing] == :south
+          initial_state[:bearing] = :east
           next
         else
-          initial_state[:bearing] = "south"
+          initial_state[:bearing] = :south
           next
         end
   
       elsif p == "r"
-        if initial_state[:bearing] == "north"
-          initial_state[:bearing] = "east"
+        if initial_state[:bearing] == :north
+          initial_state[:bearing] = :east
           next
-        elsif initial_state[:bearing] == "east"
-          initial_state[:bearing] = "south"
+        elsif initial_state[:bearing] == :east
+          initial_state[:bearing] = :south
           next
-        elsif initial_state[:bearing] == "south"
-          initial_state[:bearing] = "west"
+        elsif initial_state[:bearing] == :south
+          initial_state[:bearing] = :west
           next
         else
-          initial_state[:bearing] = "north"
+          initial_state[:bearing] = :north
           next
         end
   
       elsif p == "a"
-        if initial_state[:bearing] == "north"
+        if initial_state[:bearing] == :north
           initial_state[:y] += 1
           next
-        elsif initial_state[:bearing] == "east"
+        elsif initial_state[:bearing] == :east
           initial_state[:x] += 1
           next
-        elsif initial_state[:bearing] == "south"
+        elsif initial_state[:bearing] == :south
           initial_state[:y] -= 1
           next
         else
