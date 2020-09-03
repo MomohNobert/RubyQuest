f49eb89c64bccc8b7c1d59936ea07e327b4f3b06
MomohNobert <okhai0112@gmail.com>
2020-09-03 13:07:44 +0100

    made changes to my_split

--- Quest03/ex03/my_split.rb
+++ Quest03/ex03/my_split.rb
@@ -1,9 +1,15 @@
 def my_split(string, charset)
     string.length == 0 ? (puts "This string is empty. Check your inputs."; return;) : nil
+
+    charset_array = charset.split("")
   
-    character_array = string.split(charset)
+    character_array = string.split(Regexp.union(charset_array))
+
+    result = character_array.select do |character|
+        character != ""
+    end
   
-    return character_array
+    return result
 end
   
 print my_split("abc def gh\t!", " \t")
