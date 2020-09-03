fc97bd2729eb80e968cfb5f23a9bdf217f88ad43
MomohNobert <okhai0112@gmail.com>
2020-09-03 10:47:36 +0100

    Created ex01 and my_is_sort.rb, check if an array is sorted

---
+++ Quest03/ex01/my_is_sort.rb
@@ -0,0 +1,16 @@
+def my_is_sort(array)
+    array.map do |a|
+      if a.instance_of? Fixnum
+        next
+      else
+        puts "#{a} is not an Integer. Check your inputs."
+        return
+      end
+    end
+  
+    if array == array.sort or array == array.sort { |a,b| b <=> a}
+      return true
+    else
+      return false
+    end
+end
