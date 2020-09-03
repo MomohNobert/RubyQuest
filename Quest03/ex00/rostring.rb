8a2bb23bfd69a1955df758794fc515bffe020b76
MomohNobert <okhai0112@gmail.com>
2020-09-03 10:33:03 +0100

    Created ex00 and rostring.rb, return a string (read instructions)  after carrying the first element to the last position

--- Quest03/ex00/rostring.rb
+++ Quest03/ex00/rostring.rb
@@ -1,7 +1,10 @@
 def rostring(string)
+    string = string.split()
+  
     firstChar = string.first
     string.shift()
     string.push(firstChar)
   
+    string = string.join(" ")
     return string
 end
