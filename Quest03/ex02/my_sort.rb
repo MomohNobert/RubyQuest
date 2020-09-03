8d10dc12006cf1133a5151d462eb73515201b0c9
MomohNobert <okhai0112@gmail.com>
2020-09-03 10:54:18 +0100

    Created ex02 and my_sort.rb, receive an array and order, sort by order.

---
+++ Quest03/ex02/my_sort.rb
@@ -0,0 +1,21 @@
+def my_sort(array, order)
+    array.map do |a|
+      if a.instance_of? Fixnum
+        next
+      else
+        puts "#{a} is not an Integer. Check your inputs."
+        return
+      end
+    end
+  
+    if order == ":asc"
+      array = array.sort
+      return array
+    elsif order == ":desc"
+      array = array.sort { |a,b| b <=> a}
+      return array
+    else 
+      puts "The wrong order has been specified. Check your input."
+      return
+    end
+end
