6864ef544455c1f042de10af5b20add371441157
Momoh Nobert <okhai0112@gmail.com>
2020-09-06 08:38:50 +0100

    created ex01 and my_roman_numerals_converter.rb, changing to roman numerals

---
+++ Quest04/ex01/my_roman_numerals_converter.rb
@@ -0,0 +1,28 @@
+def roman_numerals(numerals)
+    roman_numbers = {
+    1000 => "M",  
+    900 => "CM",  
+    500 => "D",  
+    400 => "CD",
+    100 => "C",  
+    90 => "XC",  
+    50 => "L",  
+    40 => "XL",  
+    10 => "X",  
+    9 => "IX",  
+    5 => "V",  
+    4 => "IV",  
+    1 => "I",  
+    }
+  
+    n = numerals
+    roman = ""
+
+    roman_numbers.each do |value, letter|
+        roman << letter*(n / value)
+        n = n % value
+    end 
+    return roman
+end
+
+print roman_numerals(2022)
