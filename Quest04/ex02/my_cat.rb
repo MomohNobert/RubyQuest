93a5838c197aecdcf98dd7cb8f0d3f0aae7692f2
MomohNobert <okhai0112@gmail.com>
2020-09-08 16:07:44 +0100

    created Quest06 and my_christmas_tree.rb, merry christmas

--- Quest04/ex02/my_cat.rb
+++ Quest04/ex02/my_cat.rb
@@ -1,10 +1,18 @@
-def my_cat file_name
-    result = []
-    File.open(file_name) do |f|
-        f.each_with_index do |l, i|
-            result << [i, l]
+def my_cat argv
+    file_data = ""
+    # check if argv is empty and print what is piped to the command
+    if argv.size == 0
+        return STDIN.read
+    end
+    argv.each do |filename|
+        if FileTest.exist? (File.expand_path(filename))
+            file = File.read(File.expand_path(filename))
+            file_data += file
+            file_data += "\n"
+        else
+            file_data += "my cat: #{filename}: No such file or directory \n"cd
         end
     end
-    return result
+    return file_data
 end
-puts my_cat "my_cat.rb"
+puts my_cat ARGV
--- Quest06/my_christmas_tree.rb
+++ Quest06/my_christmas_tree.rb
@@ -1,6 +1,6 @@
 # ((1..20).to_a+[6]*4).each{|i|puts ('#'*i*2).center(80)};puts;puts 'Merry Christmas'.center(80)
 
-def my_christmas_tree(times, count)
+def tree(times, count)
     array = []
     (times..(2*(3+times))).step(2).take(count + 3).to_a.each do |a|
         array << a
@@ -9,11 +9,13 @@ def my_christmas_tree(times, count)
     return array[-1]
 end
   
-def iterate_count(count)
+def my_christmas_tree(count)
+    count = count[0].to_i
+    
     i = 1
     iterate = 1
     while i < count
-        current = my_christmas_tree(iterate, i)
+        current = tree(iterate, i)
         iterate = current - 2
         i += 1
     end
@@ -26,4 +28,4 @@ def iterate_count(count)
     end
 end
   
-iterate_count(5)
+my_christmas_tree ARGV
